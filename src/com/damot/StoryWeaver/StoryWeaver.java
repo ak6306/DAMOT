56751e44aa40d9ed298e36bf9e0551cf6068acef
Samriddhi Kumar <samridhi24kumar@gmail.com>
2022-03-07 21:15:06 +0530

    added story to the game

--- src/com/damot/StoryWeaver/StoryWeaver.java
+++ src/com/damot/StoryWeaver/StoryWeaver.java
@@ -23,8 +23,8 @@ public class StoryWeaver {
     public void showBits() {
         ArrayList<StoryBits> storyBits = new ArrayList<>(Arrays.asList(StoryBits.values()));
         System.out.println(ANSI_PURPLE +storyBits.stream()
-                .filter(storyBits1 -> storyBits1.getxPosition() == player.getXdistance())
-                .filter(storyBits1 -> storyBits1.getyPosition() == player.getYdistance())
+                .filter(storyBits1 -> storyBits1.getxPosition().contains(player.getXdistance()))
+                .filter(storyBits1 -> storyBits1.getyPosition().contains(player.getYdistance()))
                 .map(Object::toString).reduce("", String::concat) + ANSI_RESET);
     }
 }
